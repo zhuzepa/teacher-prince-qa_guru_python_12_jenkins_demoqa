69eefd8691c47fdbd4d9f6fe857a99be9e2a1f2b
Prince Aleksey <chirkinaa@pgk.ru>
2023-09-06 23:21:55 +0300

    add simple test

---
+++ tests/simple/test_fail.py
@@ -0,0 +1,20 @@
+def test_fail12():
+    assert False
+
+def test_fail13():
+    assert False
+
+def test_fail14():
+    assert False
+
+def test_fail15():
+    assert False
+
+def test_fail16():
+    assert False
+
+def test_fail17():
+    assert False
+
+def test_fail18():
+    assert False
