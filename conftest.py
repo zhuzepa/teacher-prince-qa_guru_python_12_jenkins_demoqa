2e27b8fd610cc3823f623675842ef9425270b460
Prince Aleksey <chirkinaa@pgk.ru>
2023-09-07 02:44:46 +0300

    update browser

--- conftest.py
+++ conftest.py
@@ -1,20 +1,15 @@
 import pytest
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+#from utils import attach
 
-
-
-# from utils import attach
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
-    browser_version = "116.0"
+    browser_version = "99.0"
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
