088e8db8e6d5d212a6810c54f11275c0fa440a43
Prince Aleksey <chirkinaa@pgk.ru>
2023-09-07 13:00:14 +0300

    update conftest

--- conftest.py
+++ conftest.py
@@ -8,6 +8,33 @@ from selene import browser, Browser, Config
 from utils import attach
 
 
+@pytest.fixture(
+    autouse=True)  # Список всех доступных парамеров https://peter.sh/experiments/chromium-command-line-switches/
+def setup_browser():
+    browser.config.window_height = 1400
+    browser.config.window_width = 1600
+    browser_version = "100.0"
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+
+    yield
+
+    browser.quit()
+
 # @pytest.fixture(scope='function', autouse=True)
 # def setup_browser():
 #     browser.config.window_width = 1400
@@ -58,25 +85,25 @@ from utils import attach
 #     from utils import attach
 
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = "100.0"
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-
-    browser = Browser(Config(driver))
-    yield browser
-
-    browser.quit()
+# @pytest.fixture(scope='function')
+# def setup_browser(request):
+#     browser_version = "100.0"
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": browser_version,
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": True
+#         }
+#     }
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options
+#     )
+#
+#     browser = Browser(Config(driver))
+#     yield browser
+#
+#     browser.quit()
